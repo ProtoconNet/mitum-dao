8fe12128cf065834e12a1d6052321795aa75bba3
soonkuk <soonkuk@gmail.com>
2023-07-25 17:26:27 +0900

    refactor: Refactored code using reusable library components

---
+++ cmds/ps.go
@@ -0,0 +1,165 @@
+package cmds
+
+import (
+	"context"
+
+	currencycmds "github.com/ProtoconNet/mitum-currency/v3/cmds"
+	currencyprocessor "github.com/ProtoconNet/mitum-currency/v3/operation/processor"
+	"github.com/ProtoconNet/mitum-dao/operation/dao"
+	"github.com/ProtoconNet/mitum-dao/operation/processor"
+	"github.com/ProtoconNet/mitum2/base"
+	"github.com/ProtoconNet/mitum2/isaac"
+	"github.com/ProtoconNet/mitum2/launch"
+	"github.com/ProtoconNet/mitum2/util"
+	"github.com/ProtoconNet/mitum2/util/hint"
+	"github.com/ProtoconNet/mitum2/util/ps"
+)
+
+var PNameOperationProcessorsMap = ps.Name("mitum-dao-operation-processors-map")
+
+func POperationProcessorsMap(pctx context.Context) (context.Context, error) {
+	var isaacParams *isaac.Params
+	var db isaac.Database
+	var opr *currencyprocessor.OperationProcessor
+	var set *hint.CompatibleSet
+
+	if err := util.LoadFromContextOK(pctx,
+		launch.ISAACParamsContextKey, &isaacParams,
+		launch.CenterDatabaseContextKey, &db,
+		currencycmds.OperationProcessorContextKey, &opr,
+		launch.OperationProcessorsMapContextKey, &set,
+	); err != nil {
+		return pctx, err
+	}
+
+	err := opr.SetCheckDuplicationFunc(processor.CheckDuplication)
+	if err != nil {
+		return pctx, err
+	}
+	err = opr.SetGetNewProcessorFunc(processor.GetNewProcessor)
+	if err != nil {
+		return pctx, err
+	}
+
+	if err := opr.SetProcessor(
+		dao.CreateDAOHint,
+		dao.NewCreateDAOProcessor(),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.ProposeHint,
+		dao.NewProposeProcessor(),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.CancelProposalHint,
+		dao.NewCancelProposalProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.RegisterHint,
+		dao.NewRegisterProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.PreSnapHint,
+		dao.NewPreSnapProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.VoteHint,
+		dao.NewVoteProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.PostSnapHint,
+		dao.NewPostSnapProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	} else if err := opr.SetProcessor(
+		dao.ExecuteHint,
+		dao.NewPostSnapProcessor(db.LastBlockMap),
+	); err != nil {
+		return pctx, err
+	}
+
+	_ = set.Add(dao.CreateDAOHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.ProposeHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.CancelProposalHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.RegisterHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.PreSnapHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.VoteHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.PostSnapHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	_ = set.Add(dao.ExecuteHint, func(height base.Height) (base.OperationProcessor, error) {
+		return opr.New(
+			height,
+			db.State,
+			nil,
+			nil,
+		)
+	})
+
+	var f currencycmds.ProposalOperationFactHintFunc = IsSupportedProposalOperationFactHintFunc
+
+	pctx = context.WithValue(pctx, currencycmds.OperationProcessorContextKey, opr)
+	pctx = context.WithValue(pctx, launch.OperationProcessorsMapContextKey, set) //revive:disable-line:modifies-parameter
+	pctx = context.WithValue(pctx, currencycmds.ProposalOperationFactHintContextKey, f)
+
+	return pctx, nil
+}
