d1fcec4d743dbbdc5bdda6aef369260ca2976694
wyuinche <wyuinche.mido@gmail.com>
2023-07-26 15:49:43 +0900

    refactor: seperate voter info and delegator info files

---
+++ types/delegator_info.go
@@ -0,0 +1,62 @@
+package types
+
+import (
+	"github.com/ProtoconNet/mitum2/base"
+	"github.com/ProtoconNet/mitum2/util"
+	"github.com/ProtoconNet/mitum2/util/hint"
+)
+
+var DelegatorInfoHint = hint.MustNewHint("mitum-dao-delegator-info-v0.0.1")
+
+type DelegatorInfo struct {
+	hint.BaseHinter
+	account   base.Address
+	delegatee base.Address
+}
+
+func NewDelegatorInfo(account base.Address, delegatee base.Address) DelegatorInfo {
+	return DelegatorInfo{
+		BaseHinter: hint.NewBaseHinter(DelegatorInfoHint),
+		account:    account,
+		delegatee:  delegatee,
+	}
+}
+
+func (r DelegatorInfo) Hint() hint.Hint {
+	return r.BaseHinter.Hint()
+}
+
+func (r DelegatorInfo) IsValid([]byte) error {
+	e := util.ErrInvalid.Errorf("invalid VoterInfo")
+
+	if err := r.BaseHinter.IsValid(nil); err != nil {
+		return e.Wrap(err)
+	}
+
+	if err := r.account.IsValid(nil); err != nil {
+		return e.Wrap(err)
+	}
+
+	if err := r.delegatee.IsValid(nil); err != nil {
+		return e.Wrap(err)
+	}
+
+	return nil
+}
+
+func (r DelegatorInfo) Bytes() []byte {
+	ba := make([][]byte, 2)
+
+	ba[0] = r.account.Bytes()
+	ba[1] = r.delegatee.Bytes()
+
+	return util.ConcatBytesSlice(ba...)
+}
+
+func (r DelegatorInfo) Account() base.Address {
+	return r.account
+}
+
+func (r DelegatorInfo) Delegatee() base.Address {
+	return r.delegatee
+}
--- types/voter_info_json.go
+++ types/voter_info_json.go
@@ -7,21 +7,21 @@ import (
 	"github.com/ProtoconNet/mitum2/util/hint"
 )
 
-type RegisterInfoJSONMarshaler struct {
+type VoterInfoJSONMarshaler struct {
 	hint.BaseHinter
 	Account    base.Address   `json:"account"`
 	Delegators []base.Address `json:"delegators"`
 }
 
 func (r VoterInfo) MarshalJSON() ([]byte, error) {
-	return util.MarshalJSON(RegisterInfoJSONMarshaler{
+	return util.MarshalJSON(VoterInfoJSONMarshaler{
 		BaseHinter: r.BaseHinter,
 		Account:    r.account,
 		Delegators: r.delegators,
 	})
 }
 
-type RegisterInfoJSONUnmarshaler struct {
+type VoterInfoJSONUnmarshaler struct {
 	Account    string   `json:"account"`
 	Delegators []string `json:"delegators"`
 }
@@ -29,7 +29,7 @@ type RegisterInfoJSONUnmarshaler struct {
 func (r *VoterInfo) DecodeJSON(b []byte, enc *jsonenc.Encoder) error {
 	e := util.StringError("failed to decode json of VoterInfo")
 
-	var u RegisterInfoJSONUnmarshaler
+	var u VoterInfoJSONUnmarshaler
 	if err := enc.Unmarshal(b, &u); err != nil {
 		return e.Wrap(err)
 	}
@@ -51,50 +51,6 @@ func (r *VoterInfo) DecodeJSON(b []byte, enc *jsonenc.Encoder) error {
 
 	}
 	r.delegators = acc
-
-	return nil
-}
-
-type DelegatorInfoJSONMarshaler struct {
-	hint.BaseHinter
-	Account   base.Address `json:"account"`
-	Delegatee base.Address `json:"delegatee"`
-}
-
-func (r DelegatorInfo) MarshalJSON() ([]byte, error) {
-	return util.MarshalJSON(DelegatorInfoJSONMarshaler{
-		BaseHinter: r.BaseHinter,
-		Account:    r.account,
-		Delegatee:  r.delegatee,
-	})
-}
-
-type DelegatorInfoJSONUnmarshaler struct {
-	Account   string `json:"account"`
-	Delegatee string `json:"delegatee"`
-}
-
-func (r *DelegatorInfo) DecodeJSON(b []byte, enc *jsonenc.Encoder) error {
-	e := util.StringError("failed to decode json of DelegatorInfo")
-
-	var u DelegatorInfoJSONUnmarshaler
-	if err := enc.Unmarshal(b, &u); err != nil {
-		return e.Wrap(err)
-	}
-
-	switch a, err := base.DecodeAddress(u.Account, enc); {
-	case err != nil:
-		return e.Wrap(err)
-	default:
-		r.account = a
-	}
-
-	switch a, err := base.DecodeAddress(u.Delegatee, enc); {
-	case err != nil:
-		return e.Wrap(err)
-	default:
-		r.delegatee = a
-	}
 
 	return nil
 }
