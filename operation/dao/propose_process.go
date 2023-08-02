1ea0687b3e7acd2d0613ffaceb4b69076c2dfc86
wyuinche <wyuinche.mido@gmail.com>
2023-08-03 00:06:14 +0900

    fix: check if proposal exists in propose preprocess

--- operation/dao/propose_process.go
+++ operation/dao/propose_process.go
@@ -86,6 +86,10 @@ func (opp *ProposeProcessor) PreProcess(
 		return nil, base.NewBaseOperationProcessReasonError("contract account not found, %s: %w", fact.Contract(), err), nil
 	}
 
+	if err := currencystate.CheckNotExistsState(state.StateKeyProposal(fact.Contract(), fact.DAOID(), fact.ProposalID()), getStateFunc); err != nil {
+		return nil, base.NewBaseOperationProcessReasonError("proposal already exists, %s, %q, %q: %w", fact.Contract(), fact.DAOID(), fact.ProposalID(), err), nil
+	}
+
 	required := map[string]common.Big{}
 
 	currencyPolicy, err := currencystate.ExistsCurrencyPolicy(fact.Currency(), getStateFunc)
